a9ba842f2de4aa132d6225fcdbd66eb51566beef
David <banshee1811@gmail.com>
2023-11-27 07:11:35 +0100

    Update show-videos-from-storage-box.blade.php

--- resources/views/livewire/video/show-videos-from-storage-box.blade.php
+++ resources/views/livewire/video/show-videos-from-storage-box.blade.php
@@ -1,15 +1,28 @@
 <div>
 
-
-
+@push('styles')
+<link href="https://vjs.zencdn.net/8.6.1/video-js.css" rel="stylesheet" />
+@endpush
+@push('scripts')
+<script src="https://vjs.zencdn.net/8.6.1/video.min.js"></script>
+@endpush
 <div class="mt-5">
     @foreach($files as $item)
     <div wire:key="{{$item}}" class="w-full">
-        <video src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4" controls>
-
-            </video>
 
 
+            <video
+            id="my-video"
+            class="video-js"
+            controls
+            preload="auto"
+            width="640"
+            height="264"
+            poster="MY_VIDEO_POSTER.jpg"
+            data-setup="{}"
+          >
+
+          <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4" />
 
 
     </div>
