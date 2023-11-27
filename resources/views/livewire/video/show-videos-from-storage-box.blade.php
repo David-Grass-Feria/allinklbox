24a12924f372cbe2e579c61a993a5b4585099801
David <banshee1811@gmail.com>
2023-11-27 07:15:42 +0100

    Update show-videos-from-storage-box.blade.php

--- resources/views/livewire/video/show-videos-from-storage-box.blade.php
+++ resources/views/livewire/video/show-videos-from-storage-box.blade.php
@@ -1,28 +1,17 @@
 <div>
 
 
-<link href="https://vjs.zencdn.net/8.6.1/video-js.css" rel="stylesheet" />
-
-
-<script src="https://vjs.zencdn.net/8.6.1/video.min.js"></script>
 
 <div class="mt-5">
     @foreach($files as $item)
     <div wire:key="{{$item}}" class="w-full">
+        <video controls>
+            <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4">
+            Your browser does not support HTML video.
+            </video>
+            <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/webm" />
 
 
-            <video
-            id="my-video"
-            class="video-js"
-            controls
-            preload="auto"
-            width="640"
-            height="264"
-            poster="MY_VIDEO_POSTER.jpg"
-            data-setup="{}"
-          >
-
-          <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4" />
 
 
     </div>
