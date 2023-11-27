7740134fd5df8ed18524db9bd67f9a30fc51ec72
David <banshee1811@gmail.com>
2023-11-27 05:54:37 +0100

    Update show-videos-from-storage-box.blade.php

--- resources/views/livewire/video/show-videos-from-storage-box.blade.php
+++ resources/views/livewire/video/show-videos-from-storage-box.blade.php
@@ -6,7 +6,7 @@
     @foreach($files as $item)
     <div wire:key="{{$item}}" class="w-full">
         <video controls loop>
-            <source src="{{ route('displayFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4">
+            <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item),'disk' => $disk]) }}" type="video/mp4">
             Your browser does not support HTML video.
             </video>
 
