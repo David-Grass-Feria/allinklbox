fbc77af095b194e303460819264fd61cd7d72f9d
David <banshee1811@gmail.com>
2023-11-30 10:44:59 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <video id="video" preload="none" playsinline controls>
                     <source src="{{ route('streamFile', ['model' => 'video', 'collection' => 'videos', 'modelId' => 8, 'filename' => 'kk6KCFfPKcQA89qvHEQaqhIWeIW7P8-metaUm9iZXJ0IEdlaXNzIGdlZ2VuIEFsZXggRmlzY2hlcl8gRGllIEVudHNjaGVpZHVuZyAoUmVpY2hlciBhbHMgZGllIEdlaXNzZW5zIEJ1Y2gpLm1wNA==-.mp4']) }}" type="video/mp4" />
-                        <source src="{{ route('streamFile', ['model' => 'video', 'collection' => 'videos', 'modelId' => 8, 'filename' => 'kk6KCFfPKcQA89qvHEQaqhIWeIW7P8-metaUm9iZXJ0IEdlaXNzIGdlZ2VuIEFsZXggRmlzY2hlcl8gRGllIEVudHNjaGVpZHVuZyAoUmVpY2hlciBhbHMgZGllIEdlaXNzZW5zIEJ1Y2gpLm1wNA==-.mp4']) }}" type="video/webm" />
+
 
                   </video>
                 <x-welcome />
