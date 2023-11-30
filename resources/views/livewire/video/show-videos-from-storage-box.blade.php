bf91813fa915f478a8d95a378832019cbbca7cf8
David <banshee1811@gmail.com>
2023-11-30 10:37:48 +0100

    Update show-videos-from-storage-box.blade.php

--- resources/views/livewire/video/show-videos-from-storage-box.blade.php
+++ resources/views/livewire/video/show-videos-from-storage-box.blade.php
@@ -8,7 +8,7 @@
 
             <video id="{{$id}}" preload="none" playsinline controls>
                 <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => 'igo9XT1oaAcaAwY4vTY9L4mQwQsXR0-metaZmxvcmVuY2UgYnkgbWlsbHMgTWFrZXVwIFJvdXRpbmUubXA0-.mp4']) }}" type="video/mp4" />
-                    <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => 'igo9XT1oaAcaAwY4vTY9L4mQwQsXR0-metaZmxvcmVuY2UgYnkgbWlsbHMgTWFrZXVwIFJvdXRpbmUubXA0-.mp4']) }}" type="video/webm" />
+
 
               </video>
         </div>
