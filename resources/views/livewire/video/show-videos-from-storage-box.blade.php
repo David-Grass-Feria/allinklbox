d7fc6e34d7a06649b65fd83e2ac204af5a59a030
David <banshee1811@gmail.com>
2023-12-01 06:42:24 +0100

    Update show-videos-from-storage-box.blade.php

--- resources/views/livewire/video/show-videos-from-storage-box.blade.php
+++ resources/views/livewire/video/show-videos-from-storage-box.blade.php
@@ -6,8 +6,10 @@
     @foreach($files as $item)
     <div wire:key="{{$item}}" class="w-full mx-auto">
 
-            <video id="{{$id}}" src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item)]) }}" playsinline controls>
-               </video>
+            <video id="{{$id}}" playsinline controls>
+                <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item)]) }}" type="video/mp4" >
+                    <source src="{{ route('streamFile', ['model' => $model, 'collection' => $collection, 'modelId' => $modelId, 'filename' => basename($item)]) }}" type="video/webm" >
+            </video>
         </div>
     @endforeach
 
