de03b7ed7cc0d75bc6282db09d46d9f47203c644
David <banshee1811@gmail.com>
2023-12-11 03:37:40 +0100

    1.0

--- database/migrations/2023_12_05_155239_create_my_passwords_table.php
+++ database/migrations/2023_12_05_155239_create_my_passwords_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('url')->nullable();
             $table->text('password')->nullable();
             $table->text('notes')->nullable();
-            $table->string('parameters')->nullable();
             $table->timestamps();
         });
     }
